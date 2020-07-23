e9e5a9dfe2bfa807197af37e6610236b9f8de466
ANTON <34216646+TonyTskh@users.noreply.github.com>
2020-07-22 22:50:49 -0700

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,21 @@
+<?php
+
+//settings
+$name = $_REQUEST['c_name'];
+$email = $_REQUEST['c_email']
+$message = $_REQUEST['c_message']
+
+//check input filds
+if(empty($name) || empty($email) || empty($message))
+{
+    echo "Please fill all fields";
+}
+else
+{
+    mail(antonhound@gmail.com, "Website Message", $message, "From : $name <$email>");
+    echo "<script type = 'text/javascript'>alert('Message sent successfully');
+    window.history.log(-1);
+    </script>" 
+}
+
+?>
